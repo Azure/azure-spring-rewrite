fb8c1cb3d9dc4e15774c32b168e4ab2b03b35f98
kaiqianyang <kaiqianyang@microsoftcom>
2023-03-08 12:00:33 +0800

    use org.openrewrite.build.recipe-library

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,9 @@
 plugins {
-    `java-library`
-    `maven-publish`
-    signing
-
-    id("nebula.maven-resolved-dependencies") version "17.3.2"
-    id("nebula.release") version "15.3.1"
-    id("io.github.gradle-nexus.publish-plugin") version "1.0.0"
-
-    id("nebula.maven-publish") version "17.3.2"
-    id("nebula.contacts") version "5.1.0"
-    id("nebula.info") version "11.1.0"
-
-    id("org.openrewrite.rewrite") version "latest.release"
+    id("org.openrewrite.build.recipe-library") version "latest.release"
 }
 
 group = "com.azure.spring.migration"
 description = "Recipe to migrate ASA"
-
-repositories {
-    mavenCentral()
-}
 
 publishing {
     repositories {
@@ -39,26 +23,13 @@ publishing {
     }
 }
 
-configure<nebula.plugin.release.git.base.ReleasePluginExtension> {
-    defaultVersionStrategy = nebula.plugin.release.NetflixOssStrategies.SNAPSHOT(project)
-}
-
 project.rootProject.tasks.getByName("final").dependsOn(project.tasks.getByName("publishAzure-spring-rewritePublicationToGitHubPackagesRepository"))
 project.rootProject.tasks.getByName("snapshot").dependsOn(project.tasks.getByName("publishAzure-spring-rewritePublicationToGitHubPackagesRepository"))
-
-nexusPublishing {
-    repositories {
-        sonatype()
-    }
-}
 
 val rewriteSpringVersion = "4.32.0"
 dependencies {
     implementation("org.openrewrite.recipe:rewrite-spring:${rewriteSpringVersion}")
     implementation("org.openrewrite:rewrite-java:latest.release")
-    testImplementation("org.openrewrite:rewrite-test:latest.release")
     testImplementation("org.openrewrite.recipe:rewrite-testing-frameworks:latest.release")
-
-    compileOnly("org.projectlombok:lombok:latest.release")
-    annotationProcessor("org.projectlombok:lombok:latest.release")
 }
+
