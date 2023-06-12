819a844899a9de342b606c79bbf81d76f07f5a5e
kaiqianyang <kaiqianyang@microsoftcom>
2023-06-12 09:49:36 +0800

    update

--- src/test/java/com/azure/spring/migration/openrewrite/java/search/FindLiteralsTest.java
+++ src/test/java/com/azure/spring/migration/openrewrite/java/search/FindLiteralsTest.java
@@ -17,11 +17,13 @@
 package com.azure.spring.migration.openrewrite.java.search;
 
 import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
 import org.openrewrite.test.RewriteTest;
 
 import static org.openrewrite.java.Assertions.java;
 
 public final class FindLiteralsTest implements RewriteTest {
+    @DocumentExample
     @Test
     void testFindLiteral() {
         rewriteRun(
